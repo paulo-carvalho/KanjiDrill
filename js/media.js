8c6139b46db1941ffe4ddce6b708b6aa3b22f79f
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2015-07-29 02:19:02 -0400

    TODO: audio on android

---
+++ js/media.js
@@ -0,0 +1,50 @@
+// Wait for device API libraries to load
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+function onDeviceReady() {
+    playAudio();
+}
+
+// Audio player
+var my_media = null;
+var mediaTimer = null;
+
+// Play audio
+function playAudio() {
+    // Create Media object from src
+    my_media = new Media(URI('../sound/lobby.wav'), onSuccess, onError);
+
+    // Play audio
+    my_media.play();
+}
+
+// Pause audio
+//
+function pauseAudio() {
+    if (my_media) {
+        my_media.pause();
+    }
+}
+
+// Stop audio
+//
+function stopAudio() {
+    if (my_media) {
+        my_media.stop();
+    }
+    clearInterval(mediaTimer);
+    mediaTimer = null;
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+    return;
+}
+
+// onError Callback
+//
+function onError(error) {
+    return;
+}
